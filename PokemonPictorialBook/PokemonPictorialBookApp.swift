cda5a6883d7feac6abee023cf9171f61573aa723
MasaharuDobashi <masaharu.dobashi@gmail.com>
2022-07-16 18:57:40 +0900

    Initial Commit

---
+++ PokemonPictorialBook/PokemonPictorialBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonPictorialBookApp.swift
+//  PokemonPictorialBook
+//
+//  Created by 土橋正晴 on 2022/07/16.
+//
+
+import SwiftUI
+
+@main
+struct PokemonPictorialBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
